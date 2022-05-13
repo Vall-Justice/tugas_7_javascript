522c5c0e3c3e331f7d54637792c89ed230e75430
Vall-Justice <104562832+Vall-Justice@users.noreply.github.com>
2022-05-14 00:06:50 +0700

    Tugas 7 niomic

---
+++ tugas7.js
@@ -0,0 +1,15 @@
+// tugas 7 niomic
+let tinggiBadan = [152, 155, 155, 172, 158, 165, 164, 165, 166, 158];
+
+for(let i = 0; i < tinggiBadan.length; i++) {
+  console.log(tinggiBadan[i]);
+}
+ 
+for(x of tinggiBadan) {
+  console.log(x);
+}
+
+/* perbedaannya yaitu kalo 
+perulangan biasa lebih panjang sintaksnya
+ dan juga agaribet kita harus manggil variablenya diikuti indexnya
+ perulangan for of gampang kita tinggal manggil variable penggantinya 
